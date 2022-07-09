52474a90ff1e6ae5e101fd0519e8e3e5013ede47
RynZzz-404 <whatsappbotz12@gmail.com>
2022-07-09 13:53:47 +0800

    P

---
+++ config.js
@@ -0,0 +1,53 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': 'sonelstore',
+}
+
+// Other
+global.owner = ['6282252285143']
+global.premium = ['6282252285143']
+global.namaowner = ['𝓡𝔂𝓪𝓷 𝓐𝓭𝓲𝓽𝔂𝓪']
+global.namabot = ['Yαɳɳ-Mԃ']
+global.packname = '© Yαɳɳ-Mԃ'
+global.author = 'Bყ 𝓡𝔂𝓪𝓷 𝓐𝓭𝓲𝓽𝔂𝓪'
+global.sessionName = 'hisoka'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/hisoka.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
